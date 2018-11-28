f1655ef65b1cbebbf04283365e91e29fca37889e
kirillbarsukov <kirillsbarsukov@gmail.com>
2018-11-28 12:59:17 -0500

    My first branch

---
+++ src/linearCrossword.java
@@ -0,0 +1,23 @@
+/**
+ * The linear crossword generator.
+ * @author Kirill Barsukov
+ * @version November 28, 2018
+ *
+ */
+import java.util.*;
+
+public class linearCrossword {
+
+    static Scanner input = new Scanner(System.in);
+    static Random random = new Random();
+    //constants
+    static final int ROWS = 10;
+    static final int MINIMUM_COLUMNS = 5;
+    static final int MAXIMUM_COLUMNS = 10;
+
+    public static void main(String[] sync) {
+
+        System.out.println("Hi");
+    }
+
+}
