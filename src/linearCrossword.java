c88b635a4dcfba287e515759151eb9b76a5d4eec
kirillbarsukov <kirillsbarsukov@gmail.com>
2018-11-28 13:22:14 -0500

    second commit

--- src/linearCrossword.java
+++ src/linearCrossword.java
@@ -16,8 +16,25 @@ public class linearCrossword {
     static final int MAXIMUM_COLUMNS = 10;
 
     public static void main(String[] sync) {
+        displayHelloMessage();
+        getFirstInformation();
 
-        System.out.println("Hi");
     }
+
+    private static void displayHelloMessage() {
+
+        System.out.println("------------------------------------------------------------");
+        System.out.println("----Hello! There is a simple linear crossword generator-----");
+        System.out.println("------------------------------------------------------------");
+        System.out.printf("The maximum length of rows is %d. However, you can chose the number of columns.\n", ROWS);
+
+    }//end of the displayHelloMessage method
+
+    private static void getFirstInformation() {
+
+        System.out.println("How many columns do you need?");
+        System.out.printf("It must be between %d and %d", MINIMUM_COLUMNS, MAXIMUM_COLUMNS);
+
+    }//end of the getFirstInformation method
 
 }
