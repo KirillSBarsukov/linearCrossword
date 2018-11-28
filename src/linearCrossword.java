0d4bbe264dacea99e13bcda0d6dbd96531995c73
kirillbarsukov <kirillsbarsukov@gmail.com>
2018-11-28 13:55:34 -0500

    Third commit.

--- src/linearCrossword.java
+++ src/linearCrossword.java
@@ -8,12 +8,13 @@ import java.util.*;
 
 public class linearCrossword {
 
+
+    //constants
+    static final int COLUMNS = 10;
+    static final int MINIMUM_ROWS = 5;
+    static final int MAXIMUM_ROWS = 10;
     static Scanner input = new Scanner(System.in);
     static Random random = new Random();
-    //constants
-    static final int ROWS = 10;
-    static final int MINIMUM_COLUMNS = 5;
-    static final int MAXIMUM_COLUMNS = 10;
 
     public static void main(String[] sync) {
         displayHelloMessage();
@@ -26,15 +27,25 @@ public class linearCrossword {
         System.out.println("------------------------------------------------------------");
         System.out.println("----Hello! There is a simple linear crossword generator-----");
         System.out.println("------------------------------------------------------------");
-        System.out.printf("The maximum length of rows is %d. However, you can chose the number of columns.\n", ROWS);
+        System.out.printf("The maximum length of columns is %d. However, you can chose the number of rows.\n", COLUMNS);
 
     }//end of the displayHelloMessage method
 
-    private static void getFirstInformation() {
+    private static byte getFirstInformation() {
+        byte numberOfRows;
+        System.out.println("How many rows do you need?");
+        System.out.printf("It must be between %d and %d\n", MINIMUM_ROWS, MAXIMUM_ROWS);
 
-        System.out.println("How many columns do you need?");
-        System.out.printf("It must be between %d and %d", MINIMUM_COLUMNS, MAXIMUM_COLUMNS);
+        do{
+            //prompt for number of worked hours of the user
+            numberOfRows = input.nextByte();
+            //get the worked hours of the user
 
+            if(numberOfRows < MINIMUM_ROWS || numberOfRows > MAXIMUM_ROWS)
+                System.out.printf("INVALID INPUT!! The number of rows MUST be must be between %d and %d\n",MINIMUM_ROWS, MAXIMUM_ROWS);
+
+        }while( numberOfRows < MINIMUM_ROWS || numberOfRows > MAXIMUM_ROWS);
+        return numberOfRows;
     }//end of the getFirstInformation method
 
 }
